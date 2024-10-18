0f156ba0a3b21b2511debdbc8ef91a4bfcabb5f1
codes-in-dark-sv <piyushohri297@gmail.com>
2024-10-18 09:48:35 +0530

    updated config

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+const nextConfig = {
     typescript: {
-      ignoreBuildErrors: true, // Disable TypeScript errors during build
+        ignoreBuildErrors: true,
     },
     eslint: {
-      ignoreDuringBuilds: true, // Disable ESLint during build (if needed)
+        ignoreDuringBuilds: true,
     },
 };
 
+export default nextConfig;
